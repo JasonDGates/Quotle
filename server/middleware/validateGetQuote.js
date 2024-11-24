787f9da47429a02905f9d0dcec652bf76441d8ff
Jason Gates <gatesjasond@gmail.com>
2024-11-24 13:53:25 -0600

    Changed from req.body to req.params

--- server/middleware/validateGetQuote.js
+++ server/middleware/validateGetQuote.js
@@ -1,5 +1,5 @@
 const validateGetQuote = (req, res, next) => {
-    const { date } = req.body;
+    const { date } = req.params;
     if (!date) {
         return res.status(400).json({ message: 'Date is required' });
     }
--- server/routes/getQuotle.js
+++ server/routes/getQuotle.js
@@ -3,9 +3,9 @@ import QuoteModel from '../model/QuotleModel.js';
 import validateGetQuote from '../middleware/validateGetQuote.js';
 const router = Router();
 
-router.get('/quote', validateGetQuote, async (req, res) => {
+router.get('/quote/:date', validateGetQuote, async (req, res) => {
     try {
-        const quote = await QuoteModel.getQuote(req.body.date)
+        const quote = await QuoteModel.getQuote(req.params.date)
         res.send(quote);
     } catch (error) {
         res.status(500).send(error);
